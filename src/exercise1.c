fa77db4d04aa44e9e0df030ce7fc9b8217c525d0
Курочкина Виктория Евгеньевна, ИП-515 <kve1207@yandex.ru>
2025-10-17 20:36:37 +0700

    1 задание

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
+    double numbers[10];
+    double sum = 0.0;
+
+    printf("Введите 10 вещественных чисел:\n");
+
+    for(int i=0; i<10; i++){
+        printf("Число %d:", i+1);
+        scanf("%lf", &numbers[i]);
+        sum+=numbers[i];
+    }
+
+    double avg = sum/10;
+    printf("%.2f\n", avg);
     return 0;
 }
