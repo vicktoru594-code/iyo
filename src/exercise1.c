b9fa92b8d2d10f718af0666149d99b00b9e5aab2
Курочкина Виктория Евгеньевна, ИП-515 <kve1207@yandex.ru>
2025-10-17 20:50:33 +0700

    2 задание

--- src/exercise1.c
+++ src/exercise1.c
@@ -11,7 +11,7 @@ int main(int argc, char** argv) {
         scanf("%lf", &numbers[i]);
         sum+=numbers[i];
     }
-
+   
     double avg = sum/10;
     printf("%.2f\n", avg);
     return 0;
--- src/exercise2.c
+++ src/exercise2.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
+    double numbers[10];
+    printf("Введите 10 вещественных чисел:\n");
+
+    for(int i=0; i<10; i++){
+        printf("Число %d:", i+1);
+        scanf("%lf", &numbers[i]);
+
+    }
+    printf("Исходный массив:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%.2f ", numbers[i]);
+    }
+    printf("\n");
+    
+    for (int i = 0; i < 10 / 2; i++) {
+        double temp = numbers[i];
+        numbers[i] = numbers[10 - 1 - i];
+        numbers[10 - 1 - i] = temp;
+    }
+    
+    printf("Инвертированный массив:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%.2f ", numbers[i]);
+    }
+    printf("\n");
     return 0;
 }
