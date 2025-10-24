bcc6f7974a80e4317c56e0507ae341fc7a92749f
Курочкина Виктория Евгеньевна, ИП-515 <kve1207@yandex.ru>
2025-10-24 07:50:51 +0700

    4 и 5 задание

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
+    double numbers[10];
+    printf("Введите 10 вещественных чисел: \n");
+
+    for(int i=0; i<10; i++){
+        printf("Число %d:", i+1);
+        scanf("%lf", &numbers[i]);
+    }
+    if (argc != 3) {
+        fprintf(stderr, "Использование: %s X K\n", argv[0]);
+        return 1;
+    }
+    
+    double X = atof(argv[1]);  
+    int K = atoi(argv[2]);    
+    
+    if (K < 0 || K >= 10) {
+        fprintf(stderr, "Ошибка: K должно быть в диапазоне 0-9\n");
+        return 1;
+    }
+    
+    
+    for (int i = 9; i > K; i--) {
+        numbers[i] = numbers[i - 1];
+    }
+
+    numbers[K] = X;
+    
+    for (int i = 0; i < 10; i++) {
+        printf("%g", numbers[i]);
+        if (i < 9) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+    
     return 0;
 }
--- src/exercise5.c
+++ src/exercise5.c
@@ -1,6 +1,47 @@
+
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+#define ROWS_A 10
+#define COLS_A 3
+#define ROWS_B 3
+#define COLS_B 10
+
+int main() {
+    double matrixA[ROWS_A][COLS_A];
+    double matrixB[ROWS_B][COLS_B];
+    double result[ROWS_A][COLS_B];
+    
+
+    for (int i = 0; i < ROWS_A; i++) {
+        for (int j = 0; j < COLS_A; j++) {
+            scanf("%lf", &matrixA[i][j]);
+        }
+    }
+
+    for (int i = 0; i < ROWS_B; i++) {
+        for (int j = 0; j < COLS_B; j++) {
+            scanf("%lf", &matrixB[i][j]);
+        }
+    }
+    
+    for (int i = 0; i < ROWS_A; i++) {
+        for (int j = 0; j < COLS_B; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < COLS_A; k++) {
+                result[i][j] += matrixA[i][k] * matrixB[k][j];
+            }
+        }
+    }
+    
+    for (int i = 0; i < ROWS_A; i++) {
+        for (int j = 0; j < COLS_B; j++) {
+            printf("%g", result[i][j]);
+            if (i != ROWS_A - 1 || j != COLS_B - 1) {
+                printf(" ");
+            }
+        }
+    }
+    printf("\n");
+    
     return 0;
 }
